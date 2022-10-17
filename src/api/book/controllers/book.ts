bce5b26a5a03f2040e826ef9781b77568bc8a99b
valadzkoaliaksei <valadzkoaliaksei@tut.by>
2022-10-17 18:13:49 +0300

    feat(*): Запрос за книгой по id

--- src/api/book/controllers/book.ts
+++ src/api/book/controllers/book.ts
@@ -3,16 +3,19 @@
  */
 import { Strapi } from "@strapi/strapi";
 import { factories } from "@strapi/strapi";
-import { BookDataType, BookType } from "../../../types/book";
+import {
+  BookDataResponseType,
+  BooksDataResponseType,
+} from "../../../types/book";
 
 export default factories.createCoreController(
   "api::book.book",
   ({ strapi }: { strapi: Strapi }) => ({
     async find(ctx) {
       ctx.query = { ...ctx.query, populate: "*" };
-      const { data }: BookDataType = await super.find(ctx);
+      const { data }: BooksDataResponseType = await super.find(ctx);
 
-      const books: BookType[] = data.map(
+      const books = data.map(
         ({
           attributes: { issueYear, rating, title, authors, images, categories },
           id,
@@ -34,6 +37,41 @@ export default factories.createCoreController(
         })
       );
       return books;
+    },
+
+    async findOne(ctx) {
+      ctx.query = { ...ctx.query, populate: "*" };
+      const { data }: BookDataResponseType = await super.findOne(ctx);
+      const book = {
+        id: data.id,
+        title: data.attributes.title,
+        rating: data.attributes.rating,
+        issueYear: data.attributes.issueYear,
+        description: data.attributes.description,
+        publish: data.attributes.publish,
+        pages: data.attributes.pages,
+        cover: data.attributes.cover,
+        weight: data.attributes.weight,
+        format: data.attributes.format,
+        ISBN: data.attributes.ISBN,
+        producer: data.attributes.producer,
+        authors: data.attributes.authors.data.length
+          ? data.attributes.authors.data.map(
+              ({ attributes }) => attributes.name
+            )
+          : null,
+        images:
+          data.attributes.images.data?.map(({ attributes }) => ({
+            url: attributes.url,
+          })) || null,
+        categories: data.attributes.categories.data.length
+          ? data.attributes.categories.data.map(
+              ({ attributes }) => attributes.name
+            )
+          : null,
+      };
+
+      return book;
     },
   })
 );
